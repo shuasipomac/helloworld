226fc7eaa2935faa6a5aca8ca793de044a35e7e6
Silvia Huasipoma Casas <167950094+shuasipomac@users.noreply.github.com>
2024-05-30 18:24:01 -0500

    Update calc_test.py

--- test/unit/calc_test.py
+++ test/unit/calc_test.py
@@ -36,6 +36,7 @@ class TestCalculate(unittest.TestCase):
         self.assertRaises(TypeError, self.calc.add, 2, object())
 
 
+
     
     def test_divide_method_fails_with_nan_parameter(self):
         self.assertRaises(TypeError, self.calc.divide, "2", 2)
